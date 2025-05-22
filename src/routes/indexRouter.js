b2607c3d3f73ddcb75ed134ed8c121de9e7d3d9e
Santiago Alfonso <salfonsop@udistrital.edu.co>
2025-05-21 19:04:14 -0500

    :zap: commit necesario

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,4 +1,3 @@
-// src/routes/indexRouter.js
 const express = require('express');
 const router = express.Router();
 const controladorVistas = require('../controlador/controladorVistas');
@@ -8,17 +7,16 @@ router.get('/', controladorVistas.paginaIndex);
 
 // Ruta /login → login.html
 router.get('/login', controladorVistas.paginaLogin);
-router.post('/login',  controladorVistas.loginProcess);
+router.post('/login', controladorVistas.loginProcess);
 
-//Ruta /registro → registro.html
-router.get('/registro',controladorVistas.paginaRegistro);
-router.post('/registro',controladorVistas.registrarUsuario);
+// Ruta /registro → registro.html
+router.get('/registro', controladorVistas.paginaRegistro);
+router.post('/registro', controladorVistas.registrarUsuario);
 
-//Ruta /r → registro.html
+// Ruta protegida /paginaMenuUser
 router.get('/paginaMenuUser', controladorVistas.paginaMenuUser);
 
-// Nueva ruta para el logout
-router.get('/logout', controladorVistas.logout); // Usando GET
-
+// Ruta para logout
+router.get('/logout', controladorVistas.logout);
 
 module.exports = router;
