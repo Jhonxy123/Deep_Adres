002bee9e1a26d1e3448f2351d858d2d0bcf0b9e9
Santiago Alfonso <salfonsop@udistrital.edu.co>
2025-05-31 15:20:29 -0500

    :sparkles: Añadiendo la API

---
+++ services/ia.service.js
@@ -0,0 +1,20 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const ai = new GoogleGenAI({ apiKey: process.env.KEY });
+
+async function main() {
+    try{
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Qué es ADRES Colombia?",
+  });
+  console.log(response.text);
+   } catch (error) {
+    console.error("❌ Error al generar contenido:", error.message || error);
+  }
+}
+
+main();
