8cd7ce31298c29adb0c5e58b61bab003cb8e4c55
Jhon Gonzalez <jhonxy123@gmail.com>
2025-06-22 22:13:23 -0500

    :zap: solucionando manejo de fecha por gemini

--- services/ia.service.js
+++ services/ia.service.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import indemnizacionDAO from '../src/modelo/DAO_indemnizacion.js';
 
+const fecha = new Date();
 const prompt = `Eres *Auditor de Indemnizaciones de la ADRES de Colombia. Recibirás como entrada un JSON que contiene la versión digital del “Formulario de radicación de personas naturales” vigente desde el 1 de octubre de 2023. Basándote **únicamente* en:
 
 * Decreto 780 de 2016 – MSPS
@@ -21,6 +22,7 @@ analiza cada campo del JSON y responde con un *informe* que incluya:
 1. *Viabilidad de indemnización* según las normas y la información provista.
 2. *Tipo de daño* reportado en el caso (por ejemplo, lesiones personales, daños materiales, eventos naturales, etc.).
 3. *Estimación del valor* que corresponde pagar como indemnización.
+4. *Especifica la fecha del dia en el que se solicito la indemnización en el inicio del informe. Recuerda que estamos en la fecha ${fecha}.
 
 No agregues información extra ni salgas del rol; limita tu análisis a lo contenido en el JSON y las normativas citadas.`;
 
